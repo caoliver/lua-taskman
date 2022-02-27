5903dd9e2a20105f42adc9adb39045045d450665
Christopher A. Oliver <caowasteland@gmail.com>
2022-02-27 15:16:44 -0500

    Simplify require_and_load API. Why repeat yourself? Make missing indices behave more like a normal library table. Provide for finding shared objects on the cpath.

--- ffi+.lua
+++ ffi+.lua
@@ -4,19 +4,38 @@
 local ffi=require 'ffi'
 ffi.cdef 'char *cdef_string'
 
-function ffi.load_with_cdefs(shared_object_name)
+local cdefs_loaded = {}
+
+function ffi.load_with_cdefs(shared_object_name, search_cpath)
+   if search_cpath then
+      shared_object_name =
+	 package.searchpath(shared_object_name, package.cpath)
+   end
    local shared_object = ffi.load(shared_object_name)
-   ffi.cdef(ffi.string(shared_object.cdef_string))
+   if not cdefs_loaded[shared_object_name] then
+      ffi.cdef(ffi.string(shared_object.cdef_string))
+      cdefs_loaded[shared_object_name] = true
+   end
    return shared_object
 end
 
-function ffi.require_and_load(library, so_file)
-   local handle = require(library)
+function ffi.require_and_load(library)
+   -- Require sees lua code too, but we just want the binary libraries.
+   local handle = assert(package.loaders[3](library))()
    if handle then
-      setmetatable(handle,
-		   {__index=ffi.load_with_cdefs(so_file)})
+      local ffilib = ffi.load_with_cdefs(library, true)
+      -- Don't crash on missing index.
+      local mt = getmetatable(ffilib)
+      local old_index = mt.__index
+      function mt.__index(tab, key)
+	 local success, value = pcall(old_index, tab,key)
+	 if success then return value end
+      end
+      setmetatable(handle, { __index = ffilib })
    end
    return handle
 end
+
+package.loaded['ffi+'] = ffi
 
 return ffi
